e5700d8985ab1061b0cddf1b8feefcf28f0d74e5
leofishman <leofishman@gmail.com>
2025-09-30 18:34:13 -0300

    feat: enhance SolanaWalletFormatter to include SOL balance display option

--- src/Plugin/Field/FieldFormatter/SolanaWalletFormatter.php
+++ src/Plugin/Field/FieldFormatter/SolanaWalletFormatter.php
@@ -5,7 +5,9 @@ namespace Drupal\solana_integration\Plugin\Field\FieldFormatter;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Url;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\solana_integration\Service\SolanaClient;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Plugin implementation of the 'solana_wallet_link' formatter.
@@ -18,7 +20,39 @@ use Drupal\Core\Url;
  *  }
  * )
  */
-class SolanaWalletFormatter extends FormatterBase {
+class SolanaWalletFormatter extends FormatterBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The Solana client service.
+   *
+   * @var \Drupal\solana_integration\Service\SolanaClient
+   */
+  protected SolanaClient $solanaClient;
+
+  /**
+   * Constructs a SolanaWalletFormatter object.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the formatter.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the formatter is associated.
+   * @param array $settings
+   *   The formatter-specific settings.
+   * @param string $label
+   *   The formatter label.
+   * @param string $view_mode
+   *   The view mode.
+   * @param array $third_party_settings
+   *   Any third party settings.
+   * @param \Drupal\solana_integration\Service\SolanaClient $solana_client
+   *   The Solana client service.
+   */
+  public function __construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings, SolanaClient $solana_client) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
+    $this->solanaClient = $solana_client;
+  }
 
   /**
    * {@inheritdoc}
@@ -29,6 +63,7 @@ class SolanaWalletFormatter extends FormatterBase {
       'trim_address' => TRUE,
       'trim_length' => 4,
       'explorer' => 'solscan',
+      'show_balance' => FALSE,
     ] + parent::defaultSettings();
   }
 
@@ -75,6 +110,12 @@ class SolanaWalletFormatter extends FormatterBase {
       ],
     ];
 
+    $elements['show_balance'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Show SOL balance'),
+      '#default_value' => $this->getSetting('show_balance'),
+      '#description' => $this->t('Display the account balance next to the address. This may impact page load time as it requires an RPC call for each address.'),
+    ];
     return $elements;
   }
 
@@ -93,6 +134,9 @@ class SolanaWalletFormatter extends FormatterBase {
       $len = $this->getSetting('trim_length');
       $summary[] = $this->t('Abbreviated to @len...@len characters', ['@len' => $len]);
     }
+    if ($this->getSetting('show_balance')) {
+      $summary[] = $this->t('Shows SOL balance');
+    }
     return $summary;
   }
 
@@ -105,6 +149,7 @@ class SolanaWalletFormatter extends FormatterBase {
     $trim = $this->getSetting('trim_address');
     $trim_length = (int) $this->getSetting('trim_length');
     $explorer = $this->getSetting('explorer');
+    $show_balance = $this->getSetting('show_balance');
 
     foreach ($items as $delta => $item) {
       $address = $item->address;
@@ -117,17 +162,40 @@ class SolanaWalletFormatter extends FormatterBase {
         $display_address = substr($address, 0, $trim_length) . '...' . substr($address, -$trim_length);
       }
 
+      $balance_text = '';
+      if ($show_balance) {
+        try {
+          $balance_array = $this->solanaClient->getBalance($address);
+          $lamports = $balance_array['value'] ?? NULL;
+          if (is_int($lamports)) {
+            $sol = $lamports / 1_000_000_000;
+            // Format SOL to a reasonable number of decimal places.
+            $formatted_sol = number_format($sol, $sol > 0.001 ? 4 : 9);
+            $balance_text = " ({$formatted_sol} SOL)";
+          }
+        }
+        catch (\Exception $e) {
+          // Fail silently, just don't show the balance.
+        }
+      }
+
       if ($link_to_explorer) {
         $url = $this->buildExplorerUrl($address, $explorer);
         $elements[$delta] = [
-          '#type' => 'link',
-          '#title' => $display_address,
-          '#url' => Url::fromUri($url),
-          '#options' => ['attributes' => ['target' => '_blank', 'title' => $this->t('View on explorer: @address', ['@address' => $address])]],
+          '#markup' => $this->t('<a href=":url" target="_blank" title="@title">:address</a>@balance', [
+            ':url' => $url,
+            '@title' => $this->t('View on explorer: @address', ['@address' => $address]),
+            ':address' => $display_address,
+            '@balance' => $balance_text,
+          ]),
         ];
       }
       else {
-        $elements[$delta] = ['#plain_text' => $display_address];
+        $elements[$delta] = [
+          '#type' => 'inline_template',
+          '#template' => '{{ address }}{{ balance }}',
+          '#context' => ['address' => $display_address, 'balance' => $balance_text],
+        ];
       }
     }
 
@@ -144,4 +212,21 @@ class SolanaWalletFormatter extends FormatterBase {
       default => "https://solscan.io/account/{$address}",
     };
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('solana_integration.client')
+    );
+  }
+
 }
